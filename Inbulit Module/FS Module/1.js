1e85afe2f3eb410ec4f8236d561a85f301a71370
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-11-19 00:44:01 +0530

    Inbuilt Modules - FS

--- Inbulit Module/FS Module/1.js
+++ Inbulit Module/FS Module/1.js
@@ -1,6 +1,7 @@
 // fs Module - To perform read and write operation 
 
-const fs = require("fs")
+const fs = require("fs")          
+// Here instead of fs we can use any variable name, and this is applicable only in default method not in named
 
 // At First, we have to create txt file in this folder which contain some txt content in it
 
